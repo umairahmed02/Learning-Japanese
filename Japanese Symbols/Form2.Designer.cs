debc92376fe7f5c75cee2f5a223b07ed8ef04632
umairahmed02 <umi1045@gmail.com>
2024-11-14 13:52:08 +0000

    added buttons for both languages (non functional)
    note to self: think about putting the lists into a 2-d list for easier refactoring of code and overall cleaner code. this might be more effort than it's worth but the concept seems promising

--- Japanese Symbols/Form2.Designer.cs
+++ Japanese Symbols/Form2.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.wlcmLbl = new System.Windows.Forms.Label();
             this.jpnWlcmLbl = new System.Windows.Forms.Label();
+            this.hiraganaButton = new System.Windows.Forms.Button();
+            this.katakanaButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // wlcmLbl
@@ -50,11 +52,32 @@
             this.jpnWlcmLbl.TabIndex = 1;
             this.jpnWlcmLbl.Text = "ウマイヤの日本語文字学習アプリケーションへようこそ";
             // 
+            // hiraganaButton
+            // 
+            this.hiraganaButton.Location = new System.Drawing.Point(249, 259);
+            this.hiraganaButton.Name = "hiraganaButton";
+            this.hiraganaButton.Size = new System.Drawing.Size(140, 23);
+            this.hiraganaButton.TabIndex = 2;
+            this.hiraganaButton.Text = "Hiragana";
+            this.hiraganaButton.UseVisualStyleBackColor = true;
+            this.hiraganaButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // katakanaButton
+            // 
+            this.katakanaButton.Location = new System.Drawing.Point(414, 259);
+            this.katakanaButton.Name = "katakanaButton";
+            this.katakanaButton.Size = new System.Drawing.Size(140, 23);
+            this.katakanaButton.TabIndex = 3;
+            this.katakanaButton.Text = "Katakana";
+            this.katakanaButton.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.katakanaButton);
+            this.Controls.Add(this.hiraganaButton);
             this.Controls.Add(this.jpnWlcmLbl);
             this.Controls.Add(this.wlcmLbl);
             this.Name = "Form2";
@@ -68,5 +91,7 @@
 
         private System.Windows.Forms.Label wlcmLbl;
         private System.Windows.Forms.Label jpnWlcmLbl;
+        private System.Windows.Forms.Button hiraganaButton;
+        private System.Windows.Forms.Button katakanaButton;
     }
 }
