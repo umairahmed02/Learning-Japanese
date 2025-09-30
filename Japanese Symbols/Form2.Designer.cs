94941138d68c4a56b85fa5202a0b1103e2e5e6de
umairahmed02 <umi1045@gmail.com>
2025-09-30 22:04:46 +0100

    linked the mouseLeave functions to Form2.Designer so they actually work

--- Japanese Symbols/Form2.Designer.cs
+++ Japanese Symbols/Form2.Designer.cs
@@ -67,6 +67,7 @@
             this.hiraganaButton.UseVisualStyleBackColor = false;
             this.hiraganaButton.Click += new System.EventHandler(this.hiraganaButton_Click);
             this.hiraganaButton.MouseHover += new System.EventHandler(this.hiraganaButton_Hover);
+            this.hiraganaButton.MouseLeave += new System.EventHandler(this.hiraganaButton_Leave);
             // 
             // katakanaButton
             // 
@@ -79,6 +80,7 @@
             this.katakanaButton.UseVisualStyleBackColor = false;
             this.katakanaButton.Click += new System.EventHandler(this.katakanaButton_Click);
             this.katakanaButton.MouseHover += new System.EventHandler(this.katakanaButton_Hover);
+            this.katakanaButton.MouseLeave += new System.EventHandler(this.katakanaButton_Leave);
             // 
             // mixedButton
             // 
@@ -91,6 +93,7 @@
             this.mixedButton.UseVisualStyleBackColor = false;
             this.mixedButton.Click += new System.EventHandler(this.mixedButton_Click);
             this.mixedButton.MouseHover += new System.EventHandler(this.mixedButton_Hover);
+            this.mixedButton.MouseLeave += new System.EventHandler(this.mixedButton_Leave);
             // 
             // difficultyTrackBar
             // 
